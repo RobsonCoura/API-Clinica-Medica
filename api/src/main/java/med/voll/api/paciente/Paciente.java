16f3576dae6d09a3ba57e4d64fc151438c3cb930
Robson Coura <robson_coura@yahoo.com.br>
2023-09-23 20:35:23 -0300

    Add método para atualizar informações no DTO DadosAtualizacaoPaciente

--- api/src/main/java/med/voll/api/paciente/Paciente.java
+++ api/src/main/java/med/voll/api/paciente/Paciente.java
@@ -37,4 +37,17 @@ public class Paciente {
         this.endereco = new Endereco(dados.endereco());
     }
 
+    //Método para atualizar informacoes
+    public void atualizarInformacoes(DadosAtualizacaoPaciente dados) {
+        //Condicao atualizar apenas se o campo estiver vindo no JSON
+        if (dados.nome() != null)
+            this.nome = dados.nome();
+
+        if (dados.telefone() != null)
+            this.telefone = dados.telefone();
+
+        if (dados.endereco() != null)
+            endereco.atualizarInformacoes(dados.endereco());
+    }
+
 }
