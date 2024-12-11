b0669e0a66ab08d312de098933fee4a87c81f315
Robson Coura <robson_coura@yahoo.com.br>
2024-12-11 17:16:05 -0300

    feat:(DadosCadastroPaciente) Add mensagens de erro nas anotacoes bean validation

--- api/src/main/java/med/voll/api/domain/paciente/DadosCadastroPaciente.java
+++ api/src/main/java/med/voll/api/domain/paciente/DadosCadastroPaciente.java
@@ -8,15 +8,15 @@ import jakarta.validation.constraints.Pattern;
 import med.voll.api.domain.endereco.DadosEndereco;
 
 public record DadosCadastroPaciente(
-        @NotBlank
+        @NotBlank(message = "Nome é obrigatório")
         String nome,
-        @NotBlank @Email
+        @NotBlank @Email(message = "Email é obrigatório")
         String email,
-        @NotBlank
+        @NotBlank(message = "Telefone é obrigatório")
         String telefone,
-        @NotBlank @Pattern(regexp = "\\d{3}\\.?\\d{3}\\.?\\d{3}\\-?\\d{2}")
+        @NotBlank(message = "CPF é obrigatório") @Pattern(regexp = "\\d{3}\\.?\\d{3}\\.?\\d{3}\\-?\\d{2}", message = "Formato do CPF é inválido")
         String cpf,
-        @NotNull @Valid
+        @NotNull(message = "Dados do endereço são obrigatórios") @Valid
         DadosEndereco endereco
 ) {
 }
