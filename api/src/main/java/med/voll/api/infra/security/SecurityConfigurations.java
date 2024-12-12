3522d7fc76f0aebf49af2c9b6502a2086ed5c51c
Robson Coura <robson_coura@yahoo.com.br>
2024-12-12 17:04:19 -0300

    feat:(SecurityConfigurations) Criacao metodo para criptografar e comparar senhas de forma segura

--- api/src/main/java/med/voll/api/infra/security/SecurityConfigurations.java
+++ api/src/main/java/med/voll/api/infra/security/SecurityConfigurations.java
@@ -1,10 +1,15 @@
 package med.voll.api.infra.security;
 
+import med.voll.api.controller.AutenticacaoController;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -21,4 +26,15 @@ public class SecurityConfigurations {
                 .build();
     }
 
+    // Cria o AuthenticationManager para gerenciar a autenticação
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception{
+        return configuration.getAuthenticationManager();
+    }
+
+    // Cria o PasswordEncoder para criptografar e comparar senhas de forma segura
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
