4f354bcc0dd0d38ad41358535db98800de7589c6
Robson Coura <robson_coura@yahoo.com.br>
2025-01-15 16:34:17 -0300

    feat: configuração da cadeia de filtros de segurança e liberação de endpoints Swagger

--- api/src/main/java/med/voll/api/infra/security/SecurityConfigurations.java
+++ api/src/main/java/med/voll/api/infra/security/SecurityConfigurations.java
@@ -1,8 +1,9 @@
 package med.voll.api.infra.security;
 
-import med.voll.api.controller.AutenticacaoController;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -11,24 +12,42 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfigurations {
 
+    @Autowired
+    private SecurityFilter securityFilter;
+
+    //Para devolvermos um objeto para o Spring, usamos a anotação @Bean
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        // Desativa a proteção CSRF, não necessária para APIs REST.
-        return http.csrf(csrf -> csrf.disable())
+        return http
+                .authorizeHttpRequests((authz) -> authz
+                        .requestMatchers(HttpMethod.POST, "/login").permitAll()
+                        //liberar endopoints swagger
+                        .requestMatchers("/v3/api-docs/**", "/swagger-ui.html", "swagger-ui/**").permitAll()
+                        .anyRequest().authenticated())
+                .httpBasic(withDefaults())
                 // Configura a API para não usar sessões (stateless).
-                .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .cors(withDefaults())
+                // Desativa a proteção CSRF, não necessária para APIs REST.
+                .csrf(csrf -> csrf.disable())
+                //estabele a ordem dos filtros - 1 do projeto, 2 do spring
+                .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
                 // Conclui e retorna o filtro de segurança.
                 .build();
+
     }
 
     // Cria o AuthenticationManager para gerenciar a autenticação
     @Bean
-    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception{
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
         return configuration.getAuthenticationManager();
     }
 
